2661963c25b4609fc8d28ff30c5d1de5286f6fde
Yinan Chen <yinanchen@Yinans-MacBook-Air.local>
2026-01-17 20:18:31 +0000

    Fix double BrowserRouter causing GitHub Pages blank screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ProjectDPS from "./pages/ProjectDPS";
@@ -19,21 +19,19 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/projects/dps" element={<ProjectDPS />} />
-          <Route path="/projects/robotics" element={<ProjectRobotics />} />
-          <Route path="/projects/virtual-keyboard" element={<ProjectVirtualKeyboard />} />
-          <Route path="/projects/emg" element={<ProjectEMG />} />
-          <Route path="/projects/allscoo" element={<ProjectAllscoo />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/projects/dps" element={<ProjectDPS />} />
+        <Route path="/projects/robotics" element={<ProjectRobotics />} />
+        <Route path="/projects/virtual-keyboard" element={<ProjectVirtualKeyboard />} />
+        <Route path="/projects/emg" element={<ProjectEMG />} />
+        <Route path="/projects/allscoo" element={<ProjectAllscoo />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
+
 
 export default App;
